4526e8ab809005e4e52cd882c7daf774cdfce0f0
Deven <devenzachuth@gmail.com>
2022-04-06 17:00:21 -0500

    commit tree 6af565f0f2ae6540f13cb17d480756a8cb76888c

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+ const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat() {
+    let cats1 = [...cats]
+    cats1.push("Broom")
+    return cats1
+}
+
+function prependCat() {
+    let cats2 = [...cats]
+    cats2.unshift("Arnold")
+    return cats2
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
